dc71c515cb13b45e3e5702f09debf7277b21d9b5
Zaibbee69 <jahanzaib12569@gmail.com>
2025-01-31 00:51:39 +0500

    Ok so Basicallity of the calculator done only functionality needs adding

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
+
+// First querying over for all the buttons and required Stuff
+let buttons = document.querySelectorAll("button");
+let result = document.getElementById("result");
+let history = document.getElementById("history");
+let clear = document.getElementById("btn-clr");
+
+// Adding an event listener if clicked
+buttons.forEach(button => {
+button.addEventListener("click", () => {
+
+    // Getting the value of the button which was clicked
+    let value = button.dataset.value;
+
+    // Now changing the display based on what was clicked
+    result.textContent += value.toString();
+})
+})
+
+// Adding an event listener if clear clicked
+clear.addEventListener("click", () => {
+
+    // Resetting values
+    result.innerHTML = "";
+    history.innerHTML = "";
+})
+
+
+
 // Ok so first making functions for the function for the operate
 function operate(num1, num2, operator)
 {
@@ -16,6 +45,7 @@ function operate(num1, num2, operator)
         // If any kind of error occurs
         default:
             return NaN;
-            break;
     }
 }
+
+// Functions for all the buttons that populate the display when clicked
